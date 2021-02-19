e69b2ba5374be260e6ccc4485a82058eed5d964d
krishnamishra2610 <67550292+krishnamishra2610@users.noreply.github.com>
2021-02-20 01:00:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const result = document.querySelector('.score')
 let currentSnake = [2, 1, 0]
 let direction = 1
 let appleId = 0
-let width = 20
+let width = 16
 let interval = 650
 let speed = 0.9
 let score = 0
@@ -19,8 +19,8 @@ function start() {
     result.textContent = score
     squares[appleId].classList.remove('apple')
     clearInterval(int)
-    appleId = Math.floor(Math.random() * 400)
-    for (j = 0; j < 400; j++) {
+    appleId = Math.floor(Math.random() * 255)
+    for (j = 0; j < 255; j++) {
         squares[j].classList.remove('head', 'snake', 'apple')
     }
     currentSnake = [2, 1, 0]
@@ -46,18 +46,18 @@ function control(e) {
 
 function move() {
 
-    if (currentSnake[0] > 399 || currentSnake[0] < 0) {
+    if (currentSnake[0] > 255 || currentSnake[0] < 0) {
         clearInterval(int)
         let inte = setTimeout(() => {
             alert('Game Over, click on Start/Restart to play again')
-        }, 1000);
+        }, 300);
 
     } else {
         squares[currentSnake[0]].classList.remove('head')
         const tail = currentSnake.pop()
         squares[tail].classList.remove('snake', 'head')
         currentSnake.unshift(currentSnake[0] + direction)
-        if (currentSnake[0] > 399 || currentSnake[0] < 0) {
+        if (currentSnake[0] > 255 || currentSnake[0] < 0) {
 
         } else {
             currentSnake.forEach(index => squares[index].classList.add('snake'))
@@ -73,7 +73,7 @@ function move() {
                 clearInterval(int)
                 let inte = setTimeout(() => {
                     alert('Game Over, click on Start/Restart to play again')
-                }, 1000);
+                }, 300);
 
             }
 
@@ -81,9 +81,9 @@ function move() {
             if (squares[currentSnake[0]].classList.contains('apple')) {
                 currentSnake.push(tail)
                 squares[currentSnake[0]].classList.remove('apple')
-                appleId = Math.floor(Math.random() * 399)
+                appleId = Math.floor(Math.random() * 255)
                 if (squares[appleId].classList.contains('snake', 'head', 'apple')) {
-                    appleId = Math.floor(Math.random() * 399)
+                    appleId = Math.floor(Math.random() * 255)
                 }
                 squares[appleId].classList.add('apple')
                 score++
@@ -97,103 +97,3 @@ function move() {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function right() {
-
-
-//     for (j = 1; j <= n; j++) {
-//         squares[i + j].classList.add('snake')
-//     }
-//     squares[i].classList.remove('snake')
-//     squares[i + n - 1].classList.remove('head')
-//     i++
-//     squares[i + n - 1].classList.add('head')
-//     headId = i + n
-//     if (headId === appleId) {
-//         squares[appleId - 1].classList.remove('head', 'snake')
-//         squares[appleId].classList.remove('apple')
-//         n++
-//         generateApple()
-//     }
-
-// }
-
-// function generateApple() {
-//     appleId = Math.floor(Math.random() * 400)
-
-//     squares[appleId].classList.add('apple')
-// }
-
-// generateApple();
-
-// controls.addEventListener('click', walk)
-
-// function walk(e) {
-//     if (e.target.id == "right") {
-//         clearInterval(interval2)
-//         interval1 = setInterval(right, 100);
-//     } else if (e.target.id == "down") {
-//         clearInterval(interval1)
-//         interval2 = setInterval(down, 100);
-//     }
-// }
-
-// function down() {
-//     for (j = 0; j <= n; j++) {
-//         squares[i + j * 20].classList.add('snake')
-//     }
-//     squares[headId].classList.remove('head')
-//     squares[headId + 20].classList.add('head')
-
-
-// }
